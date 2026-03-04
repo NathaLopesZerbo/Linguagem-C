57e5be0a2152b6288f3b823dd483ef3c0457b616
Natha Lopes Zerbo <nathalopeszerbo7@gmail.com>
2026-03-04 20:16:28 -0300

    Entendendo os placeholders dos tipos de saida da linguagem, quando se trata de inteiros, decimais e dentre outros

--- Tipos-Saida/main.c
+++ Tipos-Saida/main.c
@@ -3,16 +3,22 @@
 
 int main()
 {
-    int numInt; // printf("%d", a);
-    double numDouble; // printf("%.2lf", b);
+    int numInt;
+    double numDouble; 
+    char nome[20]; 
     char sexo;
 
-    numInt = 10;
-    numDouble = 1600.70;
-    sexo = 
+    numInt = 19;
+    numDouble = 1.70;
+    strcpy(nome, "Natha Lopes Zerbo");
+    sexo = 'M';
 
     printf("%d\n", numInt);
     printf("%.2lf\n", numDouble);
+    printf("%s\n", nome);
+    printf("%c\n", sexo);
+
+    printf("Sou %s e tenho %d anos, minha altura eh %.2lf e sou do sexo %c", nome, numInt, numDouble, sexo);
 
     return 0;
 }
