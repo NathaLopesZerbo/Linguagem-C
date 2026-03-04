68e6c90f6d002872aa63a74e278e17c88daa2a4b
Natha Lopes Zerbo <nathalopeszerbo7@gmail.com>
2026-03-04 20:04:31 -0300

    Iniciando

---
+++ Tipos-Saida/main.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include <stdio.h>
+
+int main()
+{
+    int numInt; // printf("%d", a);
+    double numDouble; // printf("%.2lf", b);
+    char sexo;
+
+    numInt = 10;
+    numDouble = 1600.70;
+    sexo = 
+
+    printf("%d\n", numInt);
+    printf("%.2lf\n", numDouble);
+
+    return 0;
+}
